306e7576a83b6da19e596d91514d2afbaffe526f
Jordan Hayes <jordanhayes@Jordans-MacBook-Pro.local>
2020-07-14 03:06:52 -0500

    redesign for me

---
+++ README.md
@@ -0,0 +1,16 @@
+# React JS Resume Web App      
+![ReactJS Resume Website Template](resume-screenshot.jpg?raw=true "ReactJS Resume Portfolio")
+### <a href="https://react-resume-template.herokuapp.com/">LIVE DEMO</a> 
+
+## Credits
+##### Udemy Course
+<a href="https://www.udemy.com/projects-in-reactjs-the-complete-react-learning-course/learn/v4/overview">Projects in ReactJS: The Complete React Learning Course by Eduonix</a>
+
+#### HTML Design Template
+<a href="https://www.styleshout.com/free-templates/ceevee/">Ceevee Template by Styleshout</a>
+
+##### Header photo credit
+<a href="https://unsplash.com/@mischievous_penguins?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge">Casey Horner</a>
+
+##### Testimonial photo credit
+<a href="https://unsplash.com/@samuelzeller?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge">Samuel Zeller</a>
---
+++ src/Components/Contact.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+
+class Contact extends Component {
+   constructor() {
+      super();
+      this.onSubmit = this.onSubmit.bind(this);
+    }
+
+
+   onSubmit(event) {
+      event.preventDefault();
+      event.target.reset();
+      emailjs.sendForm('gmail', 'template_XF1SeYSQ', event.target, 'user_XZH4pueNUstxeDPrhkt09')
+      .then((result) => {
+          console.log(result.text);
+          alert("Email Succesfully Sent! Thank you reaching out.");
+      }, (error) => {
+          console.log(error.text);
+      });
+   }
+
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+               <h1><span>Get In Touch.</span></h1>
+
+            </div>
+
+            <div className="ten columns">
+
+                  <p className="lead">{message}</p>
+
+            </div>
+
+         </div>
+
+         <div className="row">
+            <div className="eight columns">
+
+               <form className="contactForm" onSubmit={this.onSubmit}>
+                  <div>
+						   <label htmlFor="contactName">Name <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactSubject">Subject</label>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                  </div>
+
+                  <div>
+                     <button type='submit' className="submit">Submit</button>
+                     <span id="image-loader">
+                        <img alt="" src="images/loader.gif" />
+                     </span>
+                  </div>
+				   </form>
+
+           <div id="message-warning"> Error boy</div>
+				   <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+				   </div>
+           </div>
+
+
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+
+					   <h4>Address and Phone</h4>
+					   <p className="address">
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span>
+					   </p>
+				   </div>
+            </aside>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
