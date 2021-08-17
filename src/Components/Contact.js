26d4b013eaeb99b16927b95d0bdd28b0e90d48e9
jhayes99823 <hayesja99@gmail.com>
2021-08-17 11:20:06 -0500

    update resume, add new work xp, and format different files with prettier configs

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,112 +1,149 @@
-import $ from 'jquery';
-import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
-import emailjs from 'emailjs-com';
+import $ from "jquery";
+import React, { Component } from "react";
+import { findDOMNode } from "react-dom";
+import emailjs from "emailjs-com";
 
 class Contact extends Component {
-   constructor() {
-      super();
-      this.onSubmit = this.onSubmit.bind(this);
-    }
+  constructor() {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-
-   onSubmit(event) {
-      event.preventDefault();
-      emailjs.sendForm('gmail', 'template_XF1SeYSQ', event.target, 'user_XZH4pueNUstxeDPrhkt09')
-      .then((result) => {
-         this.refs["contact-form"].reset();
+  onSubmit(event) {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_XF1SeYSQ",
+        event.target,
+        "user_XZH4pueNUstxeDPrhkt09"
+      )
+      .then(
+        (result) => {
+          this.refs["contact-form"].reset();
           alert("Email Succesfully Sent! Thank you reaching out.");
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-   }
+        }
+      );
+  }
 
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
     return (
       <section id="contact">
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
+          <div className="ten columns">
+            <p className="lead">{message}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="eight columns">
+            <form
+              className="contactForm"
+              onSubmit={this.onSubmit}
+              ref="contact-form"
+            >
+              <div>
+                <label htmlFor="contactName">
+                  Name <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-         <div className="row section-head">
+              <div>
+                <label htmlFor="contactEmail">
+                  Email <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="two columns header-col">
+              <div>
+                <label htmlFor="contactSubject">Subject</label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactSubject"
+                  name="contactSubject"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-               <h1><span>Get In Touch.</span></h1>
+              <div>
+                <label htmlFor="contactMessage">
+                  Message <span className="required">*</span>
+                </label>
+                <textarea
+                  cols="50"
+                  rows="15"
+                  id="contactMessage"
+                  name="contactMessage"
+                ></textarea>
+              </div>
 
+              <div>
+                <button type="submit" className="submit">
+                  Submit
+                </button>
+                <span id="image-loader">
+                  <img alt="" src="images/loader.gif" />
+                </span>
+              </div>
+            </form>
+
+            <div id="message-warning"> Error boy</div>
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
             </div>
+          </div>
 
-            <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-                  <p className='lead' style={{ color: "white", }}>Update 10/3/2020</p>
-                  <p className='lead' style={{ color: "red", }}><b><i>If you have sent a contact message within the last 2 - 3 months I have not received your message.<br/> I apologize for the inconvenience, but the contact form is working properly now.</i></b></p>
-
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Address and Phone</h4>
+              <p className="address">
+                {name}
+                <br />
+                {street} <br />
+                {city}, {state} {zip}
+                <br />
+                <span>{phone}</span>
+              </p>
             </div>
-
-         </div>
-         <div className="row">
-            <div className="eight columns">
-
-               <form className="contactForm" onSubmit={this.onSubmit} ref="contact-form">
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button type='submit' className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-            </aside>
-      </div>
-   </section>
+          </aside>
+        </div>
+      </section>
     );
   }
 }
